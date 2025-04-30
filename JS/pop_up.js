87bf354ffea6ea36498e89d98d9b17a3434eb2a6
채수연 <suyeon030502@gmail.com>
2025-04-30 10:40:32 +0900

    java

--- JS/pop_up.js
+++ JS/pop_up.js
@@ -2,6 +2,24 @@ function pop_up() {
     window.open("../popup/pop_up.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 
+//     var close_time; // 시간 정보
+//     var close_time2 = 10; // 10초 설정
+//     clearTimeout(close_time); // 재호출 정지
+//     close_time= setTimeout("close_window()", 10000);
+//     // 1/1000 초 지정, 바로 시작
+//     show_time(); // 실시간 시간 보여주기
+
+// function show_time(){
+//     let divClock = document.getElementById('Time');
+//     divClock.innerText = close_time2; // 10초 삽입 시작
+//     close_time2--; // 1초씩 감소
+//     setTimeout(show_time, 1000); //1초마다 갱신
+//     }
+
+// function close_window() { // 함수 정의
+//     window.close(); // 윈도우 닫기
+//     }
+
 function show_clock(){
     let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
     let divClock = document.getElementById('divClock');
